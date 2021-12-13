b2ee515cf85314f3304cadb04e09aa02c203dd44
Dirk Jacobs <dirk.jacobs@supportsquare.io>
2021-12-13 14:49:18 +0100

    Added DB_PORT

--- api/scripts/createDb.js
+++ api/scripts/createDb.js
@@ -1,19 +1,19 @@
 const pgtools = require("pgtools");
-const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = require('../config.js');
+const { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD } = require('../config.js');
 
 
 const createDb = async () => {
     const host = DB_HOST;
-    const port = 5432; // MySQL: 3306
+    const port = DB_PORT;
     const user = DB_USER;
     const password = DB_PASSWORD;
 
     // create db if it doesn't already exist
     const config = {
-    user: "postgres",
+    user: user,
     host: host,
     password: password,
-    port: 5432
+    port: port
   };
   console.log("Creating DB");
   try {
