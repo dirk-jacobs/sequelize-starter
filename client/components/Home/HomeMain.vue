d863851de5967ad7255a5ddc23f6e9fb73a972a2
Dirk Jacobs <dirk.jacobs@supportsquare.io>
2021-12-16 16:21:08 +0100

    Enhanced example to use Vue Routing

---
+++ client/components/Home/HomeMain.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main">
+    <h1>{{ msg }}</h1>
+    <button @click="count++">count is: {{ count }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home-main',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
+
