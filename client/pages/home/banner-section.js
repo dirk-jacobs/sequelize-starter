dcf002953d4f3eca5c1d706b211ae56b40b76393
Dirk Jacobs <dirk.jacobs@supportsquare.io>
2021-12-20 11:54:02 +0100

    Added in Banner some ways to play with variables

--- client/pages/home/banner-section.js
+++ client/pages/home/banner-section.js
@@ -1,9 +1,22 @@
+const subTitle = 'Banner Message'; // Just to demonstrate how to use 'external' variables
+const message = "Hello world";
+
 const BannerSection = {
-  data: () => ({ message: 'Banner' }),
+  data () { 
+    this.message = message;
+    return {
+      title: 'Banner'
+    }
+  },
+  created () {
+    this.SUBTITLE = subTitle;
+  },
   template: `
   <div class="banner-section">
-    <h1>{{message}}</h1>
-    <button v-on:click="message += ' llama'">Add</button>
+    <h1>{{title}}</h1>
+    <h2>{{SUBTITLE}}</h2>
+    <p>{{message}}</p>
+    <button v-on:click="title += ' llama'">Add</button>
   </div>
   `
 }
