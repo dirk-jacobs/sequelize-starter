771c21f8ae06d3372f234d82ef557bdb8363a989
Dirk Jacobs <dirk.jacobs@supportsquare.io>
2021-12-20 10:59:29 +0100

    Added some styling

--- client/pages/home/testimonial-section.js
+++ client/pages/home/testimonial-section.js
@@ -6,17 +6,20 @@ const TestimonialSection = {
   },
   template: `
   <div class="testimonial-section">
-  <testimonial-component :testimonial="testimonials[0]" />
-  <testimonial-component :testimonial="testimonials[1]" />
-  <testimonial-component :testimonial="testimonials[2]" />
+    <template v-for="testimonial in testimonials">
+      <testimonial-component :testimonial="testimonial" />
+    </template>
   </div>
   `,
   data (){
     return {
       testimonials: [
-       {avatar: 'student1.png', content: 'Testimonial-1'},
-       {avatar: 'student1.png', content: 'Testimonial-2'},
-       {avatar: 'student1.png', content: 'Testimonial-3'},
+       {avatar: 'student1.png', 
+        content: 'Testimonial-1.....'},
+       {avatar: 'student2.png', 
+        content: 'Testimonial-2.....'},
+       {avatar: 'student3.png', 
+        content: 'Testimonial-3.....'},
       ]
     }
   }
